f1100dc41b057a3b79b13fb16999b444e828f2f8
Johannes Møgster <johannes.mogster@mojotech.no>
2019-03-17 22:16:12 +0100

    coverage test

---
+++ app.py
@@ -0,0 +1,5 @@
+import good_stuff
+
+if __name__ == "__main__":
+    four = good_stuff.two_plus_two()
+    print(four)
--- good_stuff/__init__.py
+++ good_stuff/__init__.py
@@ -1,1 +1,1 @@
-from .heihei import dummy_function_for_test
+from .heihei import dummy_function_for_test, two_plus_two
