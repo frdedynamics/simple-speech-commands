22d1168a2908e7c8ee2813146de2394602bad1c8
Johannes Møgster <johannes.mogster@mojotech.no>
2019-03-17 19:48:31 +0100

    Update test_dummy_function_for_test.py

--- test/test_dummy_function_for_test.py
+++ test/test_dummy_function_for_test.py
@@ -6,5 +6,4 @@ class TestDummyFunctionForTest(TestCase):
     def test_dummy_function_for_test(self):
         test_result = good_stuff.dummy_function_for_test()
         self.assertTrue(test_result)
-        self.assertTrue(False)
 
