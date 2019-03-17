b948859d2f81342a005b1e3a129243926f13af75
Johannes Møgster <johannes.mogster@mojotech.no>
2019-03-17 23:04:49 +0100

    more coverage! woop!

---
+++ test/test_two_plus_two.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from .context import good_stuff
+
+
+class TestTwoPlusTwo(TestCase):
+    def test_two_plus_two(self):
+        test_result = good_stuff.two_plus_two()
+        self.assertEquals(4, test_result, msg="two plus two is what? test result was: " + str(test_result))
