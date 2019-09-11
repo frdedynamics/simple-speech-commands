861ecaf7eb1da126596ddcfd9625a260df0b0624
Johannes Møgster <johannes.mogster@mojotech.no>
2019-09-11 16:48:52 +0200

    travis fail testing

--- test/test_dummy_function_for_test.py
+++ test/test_dummy_function_for_test.py
@@ -5,4 +5,4 @@ from .context import good_stuff
 class TestDummyFunctionForTest(TestCase):
     def test_dummy_function_for_test(self):
         test_result = good_stuff.dummy_function_for_test()
-        self.assertTrue(test_result)
+        self.assertFalse(test_result)
