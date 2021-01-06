f2225a4cfc2e7cc94b08f99f0f01e34d2e188cd5
Johannes Møgster <johannes.mogster@mojotech.no>
2021-01-06 10:49:36 +0100

    Update test_two_plus_two.py

--- test/test_two_plus_two.py
+++ test/test_two_plus_two.py
@@ -5,4 +5,4 @@ from .context import good_stuff
 class TestTwoPlusTwo(TestCase):
     def test_two_plus_two(self):
         test_result = good_stuff.two_plus_two()
-        self.assertEquals(4, test_result, msg="two plus two is what? test result was: " + str(test_result))
+        self.assertEqual(4, test_result, msg="two plus two is what? test result was: " + str(test_result))
